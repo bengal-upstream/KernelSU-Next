YumeMichi/KernelSU

kernel: Import susfs4ksu 1.5.9
Branch: gki-android14-6.1
Commit: https://gitlab.com/simonpunk/susfs4ksu/-/commit/646cfabce9d5dba5b806e9cbe2c198128ebd0e69

Signed-off-by: Sean Du <do4suki@gmail.com>

kernel/apk_sign.c
@@ -316,5 +316,10 @@ module_param_cb(ksu_debug_manager_uid, &expected_size_ops,
 
 bool is_manager_apk(char *path)
 {
+#ifdef CONFIG_KSU_SUSFS
+	return (check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH) ||
+			check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4")); // 5ec1cff
+#else
 	return check_v2_signature(path, EXPECTED_SIZE, EXPECTED_HASH);
+#endif
 }

bengal-upstream/KernelSU-Next

Change-Id: If936ed21c9121c7fa270b8928e7e9a136e7f6cbf
Signed-off-by: fadlyas07 <mhmmdfdlyas@proton.me>

kernel/apk_sign.c
@@ -323,5 +323,10 @@ bool is_manager_apk(char *path)
 	pr_info("%s: expected size: %u, expected hash: %s\n",
 		path, expected_manager_size, expected_manager_hash);
 
+#ifdef CONFIG_KSU_SUSFS
+	return (check_v2_signature(path, expected_manager_size, expected_manager_hash) ||
+			check_v2_signature(path, 384, "7e0c6d7278a3bb8e364e0fcba95afaf3666cf5ff3c245a3b63c8833bd0445cc4")); // 5ec1cff
+#else
 	return check_v2_signature(path, expected_manager_size, expected_manager_hash);
+#endif
 }